c13a28e1880f97d3d72f52054a3b19957b64bf79
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-06-13 19:17:40 +0300

    lesson 3.1 - 3.3 done

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,40 +1,126 @@
 
-public class Cat
+public class Cat implements Cloneable
 {
-    private double originWeight;
-    private double weight;
-
-    private double minWeight;
-    private double maxWeight;
+    private Double originWeight;
+    private Double weight;
+    private Double minWeight;
+    private Double maxWeight;
+    private double eatenFood;
+    private static int count;
 
     public Cat()
     {
-        weight = 1500.0 + 3000.0 * Math.random();
-        originWeight = weight;
-        minWeight = 1000.0;
-        maxWeight = 9000.0;
-
+        this(1500.0 + 3000.0 * Math.random());
     }
 
-    public void meow()
+    public Cat(double weight)
     {
-        weight = weight - 1;
-        System.out.println("Meow");
+        this(weight, weight, 1000.0, 9000.0, 0.0);
     }
 
-    public void feed(Double amount)
+    private Cat(double weight, double originWeight, double minWeight, double maxWeight, double eatenFood)
     {
-        weight = weight + amount;
+        this.weight = weight;
+        this.originWeight = originWeight;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+        this.eatenFood = eatenFood;
+        if(isAlive())
+          count++;
     }
 
-    public void drink(Double amount)
+    Cat(double weight, double minWeight, double maxWeight)
     {
-        weight = weight + amount;
+        this(weight, weight, minWeight, maxWeight, 0.0);
     }
+
+    public Cat(Cat other)
+    {
+        this(other.weight, other.originWeight, other.minWeight, other.maxWeight, other.eatenFood);
+    }
+
+    protected void setEatenFood (double eatenFood) {
+        if(isAlive()) {
+            this.eatenFood = eatenFood;
+        }
+    }
+
+    protected void addEatenFood (double eatenFood) {
+        setEatenFood(getEatenFood() + eatenFood);
+    }
+
+    public double getEatenFood() {
+        return eatenFood;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public void meowUntilDead() {
+       while (isAlive()) {
+           meow();
+       }
+    }
+
+    public void meow() {
+        addWeight(-1.0);
+        if(isAlive()) {
+            System.out.println("Meow");
+        }
+    }
+
+
+    public void feed(Double amount) {
+        if(isAlive())
+        {
+            addEatenFood(amount);
+            addWeight(amount);
+        }
+    }
+
+    public void drink(Double amount) {
+        addWeight(amount);
+    }
+
+    protected void setWeight(Double weight) {
+        if(isAlive()) {
+            this.weight = weight;
+            if(!isAlive()) {
+                count--;
+            }
+        }
+    }
+
+    private boolean isAlive()
+    {
+        return  getWeight() >= minWeight && getWeight() <= maxWeight;
+    }
+
+    protected void addWeight (Double weight)
+    {
+        setWeight(getWeight() + weight);
+    }
+
 
     public Double getWeight()
     {
         return weight;
+    }
+
+    public Double getOriginWeight()
+    {
+        return originWeight;
+    }
+
+    public void pee()
+    {
+        if(isAlive())
+        {
+            double shrinkage = Math.random() * 20.0;
+            addWeight(-shrinkage);
+            System.out.println("Shrinkage equal to " + shrinkage);
+        }
     }
 
     public String getStatus()
@@ -51,5 +137,27 @@ public class Cat
         else {
             return "Playing";
         }
+    }
+    public Cat copy()
+    {
+        return new Cat(this);
+    }
+
+    public static Cat copy(Cat other)
+    {
+        return new Cat(other);//other.copy();
+    }
+//------------------------------------------------------//
+    @Override
+    public Cat clone() throws CloneNotSupportedException
+    {
+        if(isAlive())
+            count++;
+        return (Cat) super.clone();
+    }
+
+    public static Cat clone(Cat other) throws CloneNotSupportedException
+    {
+        return other.clone();
     }
 }
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,42 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat();
+public class Loader {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        System.out.println("catCount = " + Cat.getCount());
+        Cat sam = new Cat();
+        System.out.println("catCount = " + Cat.getCount());
+        Cat lucky = getKitten();
+        Cat copy = sam.copy();
+        System.out.println("catCount = " + Cat.getCount());
+        Cat clone = sam.clone();
+        Cat smokey = new Cat();
+        Cat sassy = new Cat();
+        Cat oreo = new Cat();
+        System.out.println(sam.getWeight());
+        sam.meow();
+        System.out.println(sam.getWeight());
+        System.out.println(lucky.getWeight());
+        lucky.feed(356.3);
+        System.out.println(lucky.getWeight());
+        smokey.feed(7945.986);
+        smokey.meowUntilDead();
+        System.out.println(smokey.getWeight());
+        System.out.println(smokey.getStatus());
+        sassy.drink(564.45);
+        System.out.println(sassy.getStatus());
+        System.out.println(oreo.getWeight());
+        //sam.feed(9999999.9);
+        System.out.println("catCount = " + Cat.getCount());
+        sam.feed(150.00);
+        sam.pee();
+        sam.pee();
+        //sam.meow();
 
-        System.out.println(cat.getStatus());
+        System.out.println("Eaten food equal to " + sam.getEatenFood());
+    }
+
+    public static Cat getKitten()
+    {
+        double weight = 100 + 100 * Math.random();
+        return new Cat(weight, 50.0, 350.0);
     }
 }
