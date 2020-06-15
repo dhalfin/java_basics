1bafac19f3a15628a75e947eb9df24ba10eb6f9e
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-06-15 20:03:27 +0300

    hw 4.1 and 4.2 done

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,6 +1,5 @@
 
-public class Cat implements Cloneable
-{
+public class Cat implements Cloneable {
     public static final int EYES_AMOUNT = 2;
     public static final double MIN_WEIGHT = 1000.0;
     public static final double MAX_WEIGHT = 9000.0;
@@ -14,34 +13,29 @@ public class Cat implements Cloneable
     private String color;
 
 
-    public Cat()
-    {
+    public Cat() {
         this(1500.0 + 3000.0 * Math.random());
     }
 
-    public Cat(double weight)
-    {
+    public Cat(double weight) {
         this(weight, weight, 1000.0, 9000.0, 0.0);
     }
 
-    private Cat(double weight, double originWeight, double minWeight, double maxWeight, double eatenFood)
-    {
+    private Cat(double weight, double originWeight, double minWeight, double maxWeight, double eatenFood) {
         this.weight = weight;
         this.originWeight = originWeight;
         this.minWeight = minWeight;
         this.maxWeight = maxWeight;
         this.eatenFood = eatenFood;
-        if(isAlive())
-          count++;
+        if (isAlive())
+            count++;
     }
 
-    Cat(double weight, double minWeight, double maxWeight)
-    {
+    Cat(double weight, double minWeight, double maxWeight) {
         this(weight, weight, minWeight, maxWeight, 0.0);
     }
 
-    public Cat(Cat other)
-    {
+    public Cat(Cat other) {
         this(other.weight, other.originWeight, other.minWeight, other.maxWeight, other.eatenFood);
     }
 
@@ -53,13 +47,13 @@ public class Cat implements Cloneable
         this.color = color;
     }
 
-    protected void setEatenFood (double eatenFood) {
-        if(isAlive()) {
+    protected void setEatenFood(double eatenFood) {
+        if (isAlive()) {
             this.eatenFood = eatenFood;
         }
     }
 
-    protected void addEatenFood (double eatenFood) {
+    protected void addEatenFood(double eatenFood) {
         setEatenFood(getEatenFood() + eatenFood);
     }
 
@@ -72,22 +66,21 @@ public class Cat implements Cloneable
     }
 
     public void meowUntilDead() {
-       while (isAlive()) {
-           meow();
-       }
+        while (isAlive()) {
+            meow();
+        }
     }
 
     public void meow() {
         addWeight(-1.0);
-        if(isAlive()) {
+        if (isAlive()) {
             System.out.println("Meow");
         }
     }
 
 
     public void feed(Double amount) {
-        if(isAlive())
-        {
+        if (isAlive()) {
             addEatenFood(amount);
             addWeight(amount);
         }
@@ -98,80 +91,68 @@ public class Cat implements Cloneable
     }
 
     protected void setWeight(Double weight) {
-        if(isAlive()) {
+        if (isAlive()) {
             this.weight = weight;
-            if(!isAlive()) {
+            if (!isAlive()) {
                 count--;
             }
         }
     }
 
-    private boolean isAlive()
-    {
-        return  getWeight() >= minWeight && getWeight() <= maxWeight;
+    private boolean isAlive() {
+        return getWeight() >= minWeight && getWeight() <= maxWeight;
     }
 
-    protected void addWeight (Double weight)
-    {
+    protected void addWeight(Double weight) {
         setWeight(getWeight() + weight);
     }
 
 
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
 
-    public Double getOriginWeight()
-    {
+    public Double getOriginWeight() {
         return originWeight;
     }
 
-    public void pee()
-    {
-        if(isAlive())
-        {
+    public void pee() {
+        if (isAlive()) {
             double shrinkage = Math.random() * 20.0;
             addWeight(-shrinkage);
             System.out.println("Shrinkage equal to " + shrinkage);
         }
     }
 
-    public String getStatus()
-    {
-        if(weight < minWeight) {
+    public String getStatus() {
+        if (weight < minWeight) {
             return "Dead";
-        }
-        else if(weight > maxWeight) {
+        } else if (weight > maxWeight) {
             return "Exploded";
-        }
-        else if(weight > originWeight) {
+        } else if (weight > originWeight) {
             return "Sleeping";
-        }
-        else {
+        } else {
             return "Playing";
         }
     }
-    public Cat copy()
-    {
+
+    public Cat copy() {
         return new Cat(this);
     }
 
-    public static Cat copy(Cat other)
-    {
+    public static Cat copy(Cat other) {
         return new Cat(other);//other.copy();
     }
-//------------------------------------------------------//
+
+    //------------------------------------------------------//
     @Override
-    public Cat clone() throws CloneNotSupportedException
-    {
-        if(isAlive())
+    public Cat clone() throws CloneNotSupportedException {
+        if (isAlive())
             count++;
         return (Cat) super.clone();
     }
 
-    public static Cat clone(Cat other) throws CloneNotSupportedException
-    {
+    public static Cat clone(Cat other) throws CloneNotSupportedException {
         return other.clone();
     }
 }
--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,43 @@
+import java.util.zip.DeflaterOutputStream;
+
 public class Main
 {
     public static void main(String[] args)
     {
         Container container = new Container();
         container.count += 7843;
+        Byte d1 = Byte.MIN_VALUE;
+        Byte d2 = Byte.MAX_VALUE;
+        Integer d3 = Integer.MIN_VALUE;
+        Integer d4 = Integer.MAX_VALUE;
+        Short d5 = Short.MIN_VALUE;
+        Short d6 = Short.MAX_VALUE;
+        Long d7 = Long.MIN_VALUE;
+        Long d8 = Long.MAX_VALUE;
+        Float d9 = Float.MIN_VALUE;
+        Float d10 = Float.MAX_VALUE;
+        Double d11 = Double.MIN_VALUE;
+        Double d12 = Double.MAX_VALUE;
 
+        System.out.print("Byte: " + d1 + " " + d2 + "\n"
+                + "Integer: " + d3 + " " + d4 + "\n"
+                + "Short: " + d5 + " " + d6 + "\n"
+                + "Long: " + d7 + " " + d8 + "\n"
+                + "Float: "+ d9 + " " + d10 + "\n"
+                + "Double: "+ d11 + " " + d12);
     }
 
     public Integer sumDigits(Integer number)
     {
-        //@TODO: write code here
-        return 0;
+        String digits = Integer.toString(number);
+        Integer sum = 0;
+        for (int i = 0; i <=digits.length() - 1; i++) {
+            //sum += Integer.parseInt(String.valueOf(digits.charAt(i)));
+            if (Character.isDigit(digits.charAt(i))) {
+                Integer y = Character.getNumericValue(digits.charAt(i));
+                sum += y;
+            }
+        }
+        return sum;
     }
 }
