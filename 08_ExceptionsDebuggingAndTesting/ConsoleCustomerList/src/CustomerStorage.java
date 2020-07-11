d51b22291ff614a1d1786dc06ee982341dcabdd7
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-07-11 22:08:25 +0300

    hw 8.4 and 8.8 done

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,33 +1,37 @@
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
         String name = components[0] + " " + components[1];
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Неверный формат ввода.\nBeрный формат: add Василий Петров vasiliy.petrov@gmail.com +79261234567");
+        }
+        if (!Pattern.compile(".+@.+\\..+").matcher(components[2]).matches()) {
+            throw new IllegalArgumentException("Электронная почта некорректного вида, введите почту правильно aaaa@aaa.aa");
+        }
+        if (!Pattern.compile("\\+7\\d{10}").matcher(components[3]).matches()) {
+            throw new IllegalArgumentException("Номер телефона не соответсвует формату, введите телефон правильно +71111111111 ");
+        }
         storage.put(name, new Customer(name, components[3], components[2]));
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
---
+++ 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -0,0 +1,103 @@
+import core.Line;
+import core.Station;
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RouteCalculatorTest extends TestCase {
+
+    public RouteCalculator routeCalculator;
+    public StationIndex stationIndex;
+
+    public List<Station> stations;
+    public List<Station> noConnection;
+    public List<Station> oneConnection;
+    public List<Station> twoConnection;
+    public List<Station> actualResultStationList;
+
+    public Line line1;
+    public Station firstStop;
+    public Station secondStop;
+    public Station thirdStop;
+
+    public Line line2;
+    public Station fourthStop;
+    public Station fifthStop;
+    public Station sixthStop;
+
+    public Line line3;
+    public Station seventhStop;
+    public Station eighthStop;
+    public Station ninthStop;
+
+    @BeforeClass
+    public void setUp() {
+
+        line1 = new Line(1, "line1");
+        firstStop = new Station("station1", line1);
+        secondStop = new Station("station2", line1);
+        thirdStop = new Station("station3", line1);
+
+        line2 = new Line(2, "line2");
+        fourthStop = new Station("station4", line2);
+        fifthStop = new Station("station5", line2);
+        sixthStop = new Station("station6", line2);
+
+        line3 = new Line(3, "line3");
+        seventhStop = new Station("station7", line3);
+        eighthStop = new Station("station8", line3);
+        ninthStop = new Station("station9", line3);
+
+        routeCalculator = new RouteCalculator(initStationIndex());
+    }
+
+    @Test
+    public void testCalculateDuration() {
+        stations = Stream.of(firstStop, secondStop, thirdStop, fourthStop, fifthStop, sixthStop, seventhStop, eighthStop, ninthStop)
+                .collect(Collectors.toList());
+        double expected = RouteCalculator.calculateDuration(stations);
+        double actual = 22;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetShortRoutes() {
+        noConnection = Stream.of(firstStop, secondStop, thirdStop)
+                .collect(Collectors.toList());
+        actualResultStationList = routeCalculator.getShortestRoute(firstStop, thirdStop);
+        assertEquals(noConnection, actualResultStationList);
+    }
+
+    @Test
+    public void testGetShortRoutesOneConnection() {
+        oneConnection = Stream.of(firstStop, secondStop, thirdStop, sixthStop, fifthStop)
+                .collect(Collectors.toList());
+        actualResultStationList = routeCalculator.getShortestRoute(firstStop, fifthStop);
+        assertEquals(oneConnection, actualResultStationList);
+    }
+
+    @Test
+    public void testGetShortRoutesTwoConnection() {
+        twoConnection = Stream.of(firstStop, secondStop, thirdStop, sixthStop, fifthStop, fourthStop, seventhStop, eighthStop)
+                .collect(Collectors.toList());
+        actualResultStationList = routeCalculator.getShortestRoute(firstStop, eighthStop);
+        assertEquals(twoConnection, actualResultStationList);
+    }
+
+    public StationIndex initStationIndex() {
+        stationIndex = new StationIndex();
+        Stream.of(firstStop, secondStop, thirdStop, fourthStop, fifthStop, sixthStop, seventhStop, eighthStop, ninthStop)
+                .peek(e -> e.getLine().addStation(e)).forEach(stationIndex::addStation);
+        List<Station> connect1 = Arrays.asList(thirdStop, sixthStop);
+        List<Station> connect2 = Arrays.asList(fourthStop, seventhStop);
+        stationIndex.addConnection(connect1);
+        stationIndex.addConnection(connect2);
+        Stream.of(line1, line2, line3).forEach(stationIndex::addLine);
+        return stationIndex;
+    }
+}
